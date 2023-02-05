45f18eb0eba26c1353f83f7fb70bc0b66fd83c39
kazuinaba <ec2-user@ip-172-31-17-79.ap-northeast-1.compute.internal>
2023-02-05 02:34:34 +0000

    会員登録(簡易)を追加

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CompletedTaskController;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Admin\AuthController as AdminAuthController;
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Admin\UserController as AdminUserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,20 +21,44 @@ use App\Http\Controllers\TestController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-//タスク管理システム
+
+// タスク管理システム
 Route::get('/', [AuthController::class, 'index'])->name('front.index');
 Route::post('/login', [AuthController::class, 'login']);
-
 // 認可処理
 Route::middleware(['auth'])->group(function () {
-    Route::get('/task/list', [TaskController::class, 'list']);
+    Route::prefix('/task')->group(function () {
+        Route::get('/list', [TaskController::class, 'list']);
+        Route::post('/register', [TaskController::class, 'register']);
+        Route::get('/detail/{task_id}', [TaskController::class, 'detail'])->whereNumber('task_id')->name('detail');
+        Route::get('/edit/{task_id}', [TaskController::class, 'edit'])->whereNumber('task_id')->name('edit');
+        Route::put('/edit/{task_id}', [TaskController::class, 'editSave'])->whereNumber('task_id')->name('edit_save');
+        Route::delete('/delete/{task_id}', [TaskController::class, 'delete'])->whereNumber('task_id')->name('delete');
+        Route::post('/complete/{task_id}', [TaskController::class, 'complete'])->whereNumber('task_id')->name('complete');
+        Route::get('/csv/download', [TaskController::class, 'csvDownload']);
+    });
+    //
     Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/completed_tasks/list', [CompletedTaskController::class, 'list']);
+    Route::post('/user/register', [UserController::class, 'register']);
+    Route::get('/user/index', [UserController::class, 'index']);
 });
 
-//テスト用
+
+// 管理画面
+Route::prefix('/admin')->group(function () {
+    Route::get('', [AdminAuthController::class, 'index'])->name('admin.index');
+    Route::post('/login', [AdminAuthController::class, 'login'])->name('admin.login');
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::get('/top', [AdminHomeController::class, 'top'])->name('admin.top');
+        Route::get('/user/list', [AdminUserController::class, 'list'])->name('admin.user.list');
+    });
+    Route::get('/logout', [AdminAuthController::class, 'logout']);
+});
+
+// テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
-
-//form入力テスト用
+// form入力テスト用
 Route::get('/test', [TestController::class, 'index']);
 Route::post('/test/input', [TestController::class, 'input']);
